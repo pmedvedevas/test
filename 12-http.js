352b790713b6a498e9c0983d759a3b6e74389277
Povilas Medvedevas <pdmedvedevas@gmail.com>
2021-12-24 22:48:34 +0200

    first commit

---
+++ 12-http.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our homepage");
+  } else if (req.url === "/about") {
+    res.end("Here is our short history");
+  } else {
+    res.end(
+      `<h1>Ooops!</h1><p>We can't seem to find the page that you are looking for</p> <a href="/">back home</a>`
+    );
+  }
+});
+
+server.listen(5000);
---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+// Modules
+const { john, peter } = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alt-flavor");
+require("./7-mind-grenade");
+sayHi("Susan");
+sayHi(john);
+sayHi(peter);
